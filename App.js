76bec842c074f39b6d77a34c2c6f5ac47e220fab
Rubenvet <ruben.robles@uabc.edu.mx>
2022-01-06 18:43:50 -0800

    Add files via upload

--- App.js
+++ App.js
@@ -1,12 +1,15 @@
-import './App.css';
-import TodoList from './TodoList';
+import React, { Component } from "react";
+import Game from "./Game";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <TodoList />
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className='App'>
+        <Game />
+      </div>
+    );
+  }
 }
 
 export default App;
